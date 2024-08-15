a3375c619262f95fc2f664f228083c208f7722d9
narekhovzealous <narek.hov.zealous@gmail.com>
2024-08-15 10:06:15 +0400

    Add github workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
-  }
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  base: '/gold-digger/'
 });
